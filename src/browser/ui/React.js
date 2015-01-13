980073cc6a50e229391d4edf8f6bda42b17e2567
Kevin Sawicki <kevinsawicki@gmail.com>
2015-01-12 18:28:00 -0800

    Remove dev tools message
    Closes atom/atom#4991
    
    Conflicts:
    	src/browser/ui/React.js

--- src/browser/ui/React.js
+++ src/browser/ui/React.js
@@ -101,18 +101,6 @@ if (
 if (__DEV__) {
   var ExecutionEnvironment = require('ExecutionEnvironment');
   if (ExecutionEnvironment.canUseDOM && window.top === window.self) {
-
-    // If we're in Chrome, look for the devtools marker and provide a download
-    // link if not installed.
-    if (navigator.userAgent.indexOf('Chrome') > -1) {
-      if (typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ === 'undefined') {
-        console.debug(
-          'Download the React DevTools for a better development experience: ' +
-          'http://fb.me/react-devtools'
-        );
-      }
-    }
-
     var expectedFeatures = [
       // shims
       Array.isArray,
